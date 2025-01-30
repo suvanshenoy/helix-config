7faeea8071b86beb6d153fd9e845ff54d2b7fcd9
suvanshenoy <suvanshenoy155@gmail.com>
2025-01-30 17:01:49 +0530

    Add helix-config

--- run-git.sh
+++ run-git.sh
@@ -2,5 +2,5 @@ git init .
 git branch -m main
 git add .
 git commit -m "Add helix-config"
-git remote add origin https://github.com/suvanshenoy/helix-config
-git push origin main
+# git remote add origin https://github.com/suvanshenoy/helix-config
+git push origin -f main
